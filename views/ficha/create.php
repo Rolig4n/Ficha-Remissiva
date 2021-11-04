4739e4068ede1cfcfa6a9e1fb3540b4a841ebe60
Hiago R Batista <hiago@walmarcorp.com.br>
2021-11-04 19:24:25 -0300

    alteração design

--- views/ficha/create.php
+++ views/ficha/create.php
@@ -5,8 +5,8 @@ use yii\helpers\Html;
 /* @var $this yii\web\View */
 /* @var $model app\models\FchFichaRemissiva */
 
-$this->title = 'Create Fch Ficha Remissiva';
-$this->params['breadcrumbs'][] = ['label' => 'Fch Ficha Remissivas', 'url' => ['index']];
+$this->title = 'Cadastro Ficha Remissiva';
+$this->params['breadcrumbs'][] = ['label' => 'Ficha Remissivas', 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="fch-ficha-remissiva-create">
--- views/ficha/index.php
+++ views/ficha/index.php
@@ -6,7 +6,7 @@ use yii\widgets\Pjax;
 /* @var $this yii\web\View */
 /* @var $dataProvider yii\data\ActiveDataProvider */
 
-$this->title = 'Fch Ficha Remissivas';
+$this->title = 'Ficha Remissivas';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="fch-ficha-remissiva-index">
@@ -14,7 +14,7 @@ $this->params['breadcrumbs'][] = $this->title;
     <h1><?= Html::encode($this->title) ?></h1>
 
     <p>
-        <?= Html::a('Create Fch Ficha Remissiva', ['create'], ['class' => 'btn btn-success']) ?>
+        <?= Html::a('Cadastrar Ficha Remissiva', ['create'], ['class' => 'btn btn-success']) ?>
     </p>
 
     <?php Pjax::begin(); ?>
